f510c881c96e89ca20c73bc487cfb520549283a5
Aurix <Aurix@Neptune>
2012-04-07 23:25:21 -0400

    Solve question 31.

---
+++ src/solutions/Problem0031.java
@@ -0,0 +1,34 @@
+package solutions;
+
+/**
+ * Project Euler Solution Problem 0031
+ * 
+ * @author Alastair Games
+ * License: MIT
+ */
+public class Problem0031 {
+    
+    private static final int VALUE = 200;
+    
+    private static final int[] DENOMINATION = {1,2,5,10,20,50,100,200};
+    
+    public static void main(String[] args) {
+        System.out.println(countCombination(VALUE, DENOMINATION.length-1));
+    }
+    
+    private static int countCombination(int v, int d){
+        if (v == 0)
+            return 1;
+        
+        if (d == -1)
+            return 0;
+            
+        int count = 0;
+        int nCoins = v/DENOMINATION[d];
+            
+        for (int i = nCoins; i >= 0; i--)
+            count += countCombination(v-i*DENOMINATION[d],d-1);
+        
+        return count;
+    }
+}
