9e9d24bd452d80133959981b7226c8d628d8e8e3
Aurix <Aurix@Neptune>
2012-04-08 23:37:16 -0400

    Collapse two branches into one with the || operator.

--- src/solutions/Problem0001.java
+++ src/solutions/Problem0001.java
@@ -16,7 +16,7 @@ public class Problem0001 {
     public static void main(String[] args){
         int sum = 0;
         for (int i = 1; i < 1000; i++){
-            sum += i % 3 == 0 ? i : (i % 5 == 0 ? i : 0);
+            sum += (i % 3 == 0||i % 5 == 0) ? i : 0;
         }
         System.out.println(sum);
     }
